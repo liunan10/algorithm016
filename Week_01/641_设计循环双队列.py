8f50aec1a59f42b12c486b392f6795de9222a1d7
Jianpeng Xu <jianpengxu@Jianpengs-MacBook-Pro.local>
2020-09-13 21:16:47 -0700

    “week1”

---
+++ Week_01/641_设计循环双队列.py
@@ -0,0 +1,85 @@
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the deque to be k.
+        """
+        self.front, self.rear = 0, 0
+        self.capacity = k + 1
+        self.cirQue = [0 for _ in range(self.capacity)]
+
+    def insertFront(self, value: int) -> bool:
+        """
+        Adds an item at the front of Deque. Return true if the operation is successful.
+        """
+        if self.isFull(): return False
+        self.front = (self.front - 1) % self.capacity
+        self.cirQue[self.front] = value
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        """
+        Adds an item at the rear of Deque. Return true if the operation is successful.
+        """
+        if self.isFull(): return False
+        self.cirQue[self.rear] = value
+        self.rear = (self.rear + 1) % self.capacity
+        return True
+
+    def deleteFront(self) -> bool:
+        """
+        Deletes an item from the front of Deque. Return true if the operation is successful.
+        """
+        if self.isEmpty(): return False
+        self.front = (self.front + 1) % self.capacity
+        return True
+        
+
+    def deleteLast(self) -> bool:
+        """
+        Deletes an item from the rear of Deque. Return true if the operation is successful.
+        """
+        if self.isEmpty(): return False
+        self.rear = (self.rear - 1) % self.capacity
+        return True
+
+        
+
+    def getFront(self) -> int:
+        """
+        Get the front item from the deque.
+        """
+        if self.isEmpty(): return -1
+        return self.cirQue[self.front]
+        
+
+    def getRear(self) -> int:
+        """
+        Get the last item from the deque.
+        """
+        if self.isEmpty(): return -1
+        return self.cirQue[self.rear - 1]
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular deque is empty or not.
+        """
+        return self.front == self.rear
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular deque is full or not.
+        """
+        return self.front == (self.rear + 1) % self.capacity
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
